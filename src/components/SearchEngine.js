d9c9def8905b61d36b4fba51b241a6daceef1386
bbbb <114340148+bosingyim@users.noreply.github.com>
2024-10-08 09:48:32 +0700

    Update SearchEngine.js

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -13,7 +13,7 @@ function SearchEngine({ query, setQuery, search }) {
       <input
         type="text"
         className="city-search"
-        placeholder="enter city name"
+        placeholder="ใส่ชื่อเมือง"
         name="query"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
